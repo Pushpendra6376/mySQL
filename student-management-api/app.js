6d715a4f6f17fe3b11469e77e801a0cfc83a0848
pushpendra6376 <pp5395021@gmail.com>
2025-11-16 15:43:10 +0530

    created db with sequelize

--- student-management-api/app.js
+++ student-management-api/app.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const db = require('./utils/db-collection')
 const StudentsRoutes = require('./routes/studentRoute')
+
+const studentModel = require('./models/students')
 
 const app = express();
 app.use(express.json());
 
 app.use('/students', StudentsRoutes);
 
-app.listen(3000, () => {
+db.sync({force:true}).then(()=>{
+    app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+}).catch((error)=>{
+    console.log(err);
+})
+
--- student-management-api/utils/db-collection.js
+++ student-management-api/utils/db-collection.js
@@ -1,39 +1,72 @@
-const mysql = require('mysql2');
+//connecting to the server with sequelize
 
-const connection = mysql.createConnection({
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    dialect: 'mysql'
 });
 
-// create connection
-connection.connect((err) => {
-    if (err) {
-        console.log("Database connection error:", err);
-        return;
-    }
+(async ()=>{ try {
+    await sequelize.authenticate();
+    console.log("connection to the database has been created");
+} catch (error) {
+    console.log(error);     
+}})();
 
-    console.log("MySQL Connected Successfully");
+module.exports = sequelize;
 
-    // Create students table
-    const creationQuery = `
-        CREATE TABLE IF NOT EXISTS students(
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(50),
-            email VARCHAR(50) UNIQUE,
-            age INT
-        )
-    `;
 
-    connection.execute(creationQuery, (err) => {
-        if (err) {
-            console.log("Table creation error:", err);
-            connection.end();
-            return;
-        }
-        console.log("Students table ready");
-    });
-});
 
-module.exports = connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME
+// });
+
+// // create connection
+// connection.connect((err) => {
+//     if (err) {
+//         console.log("Database connection error:", err);
+//         return;
+//     }
+
+//     console.log("MySQL Connected Successfully");
+
+//     // Create students table
+//     const creationQuery = `
+//         CREATE TABLE IF NOT EXISTS students(
+//             id INT AUTO_INCREMENT PRIMARY KEY,
+//             name VARCHAR(50),
+//             email VARCHAR(50) UNIQUE,
+//             age INT
+//         )
+//     `;
+
+//     connection.execute(creationQuery, (err) => {
+//         if (err) {
+//             console.log("Table creation error:", err);
+//             connection.end();
+//             return;
+//         }
+//         console.log("Students table ready");
+//     });
+// });
+
+// module.exports = connection;
